e31e235d9b9fc65bf5af7965874f548711fd22a9
JoaoPaulo <joao.silva@mgmcorp.com.br>
2016-03-17 11:35:19 -0300

    Classe Cliente

---
+++ Camada/DTO/Pessoa/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.Pessoa
+{
+    class Cliente : Pessoa
+    {
+        public char tipoPessoa { get; set; }
+    }
+}
