4ca7491af3702a9493f8890d6a6f619944c9edf0
JoaoPaulo <joao.silva@mgmcorp.com.br>
2016-03-17 14:00:42 -0300

    Inserção de Sobrecarga do Método ToString()

--- Camada/DTO/Pessoa/Cliente.cs
+++ Camada/DTO/Pessoa/Cliente.cs
@@ -9,5 +9,11 @@ namespace DTO.Pessoa
     public class Cliente : Pessoa
     {
         public string tipoCliente { get; set; }
+
+        public override string ToString()
+        {
+            //return "Id: " + id + " Nome: " + nome;
+            return string.Format("Id: {0} - Nome: {1}", this.id, this.nome);
+        }
     }
 }
