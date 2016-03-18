bbb555b66d0c92a2b128008fc6835257d13c493b
JPSilvaGcode <jpsilvafla@gmail.com>
2016-03-18 08:03:12 -0300

    Comentário Classe Pessoa
    Comentario Email

--- Camada/DTO/Pessoa/Pessoa.cs
+++ Camada/DTO/Pessoa/Pessoa.cs
@@ -12,6 +12,6 @@ namespace DTO.Pessoa
         public string nome { get; set; }
         public string cpf { get; set; }
         public string rg { get; set; }
-        public string Email { get; set; }
+        public string Email { get; set; } //Atributo novo
     }
 }
