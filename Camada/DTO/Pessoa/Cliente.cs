395253622e41cbe2b3c6c19c665c7cbad11f5ec1
JoaoPaulo <joao.silva@mgmcorp.com.br>
2016-03-18 14:39:28 -0300

    Alteração Médito ToString()

--- Camada/DTO/Pessoa/Cliente.cs
+++ Camada/DTO/Pessoa/Cliente.cs
@@ -13,7 +13,7 @@ namespace DTO.Pessoa
 
         public override string ToString()
         {
-            return string.Format("Id: {0} - Nome: {1} - CPF: {2}", id, nome, cpf);
+            return string.Format("Id: {0} - Nome: {1} - CPF: {2} - Tipo: {3}", id, nome, cpf, tipoCliente);
         }
     }
 }
