524df615347bf48062c043d466f1d09053c6b740
Tiago <tiago.valim@mgmcorp.com.br>
2016-03-23 10:19:33 -0300

    Add Classe Representante

---
+++ Camada/DTO/Pessoa/Representante.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.Pessoa
+{
+    public class Representante : Pessoa
+    {
+        public string tipo { get; set; }
+    }
+}
